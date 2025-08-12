9e176245e82c082ccaf82c7baca6bc8aa67653b6
pratikshapotdar <potdarpratiksha2002@gmail.com>
2025-08-12 23:30:35 +0530

    new

---
+++ list.py
@@ -0,0 +1,23 @@
+#accessing list ele
+#list1=[10,20,30]
+#print(list1[0])
+#print(list1[1])
+#print(list1[2])
+
+#accessing list elements using a while
+#i = 0
+#while i<3:
+#    print(list1[i])
+#    i = i+1
+
+#using foreach 
+#for ele in list1:
+#    print(ele)
+    
+list1 = [10,25.66,"ravi"]
+print(list1[0]) #o\p: 10
+print(list1[-1]) #o\p: ravi
+print(list1[1]) #o\p: 25.66 
+print(list1[-2]) #o\p: 25.66
+print(list1[-3]) #o\p: 10
+print(list1[0:2]) #o\p: [10, 25.66]
